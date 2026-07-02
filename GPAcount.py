518791a38b81c07e5a5787935f4d60e68d2fa9d5
unknown <hohaidang225@gmail.com>
2026-07-03 00:33:18 +0700

    fix input bugs  and add some details

--- GPAcount.py
+++ GPAcount.py
@@ -1,19 +1,21 @@
 tinchitichluy=0
 diemtichluy=0
-
+count=1
+final=0
 
 while True:
     #Nhap input
     while True:
         try:
-            QTH=float(input("Diem qua trinh hoc: "))
-            THI=float(input("Diem thi: "))
-            TINCHI=int(input("Tin chi mon hoc: "))
+            print(f"mon thu {count}:")
+            QTH=float(input("Diem qua trinh hoc: ")).strip()
+            THI=float(input("Diem thi: ")).strip()
+            TINCHI=float(input("Tin chi mon hoc: ")).strip()
             break
         except ValueError:
             print("ERROR, please try again")
     #Tinh toan
-    if QTH >=0 and THI>=0:
+    if QTH >=0 and THI>=0 and TINCHI>0:
         tinchitichluy=tinchitichluy+TINCHI
         TONG =(QTH + THI)/2
         if TONG >= 8.5:
@@ -34,13 +36,23 @@ while True:
             TONG=0
         DIEM=(TONG*TINCHI)
         diemtichluy=diemtichluy+DIEM
-
-        ask=input("done?(an yes neu xong hoac an ngau nhien 1 phim neu chua xong)").strip().lower()
-        #in ket qua
-        if ask =="yes":
-            print("GPA cua ban la: ",diemtichluy/tinchitichluy)
-            print("TONG TIN CHI DAT DUOC:",tinchitichluy)
+        
+        while True:
+            ask=str(input("done?(an YES neu xong hoac NO neu chua xong): ")).strip().lower()
+            #in ket qua
+            if ask =="yes":
+                print("GPA cua ban la: ",diemtichluy/tinchitichluy)
+                print("TONG TIN CHI DAT DUOC:",tinchitichluy)
+                final=1
+                break
+            elif ask=="no":
+                count+=1
+                break
+            else:
+                print("ERROR,please try again")
+        if final==1:
             break
-        else:
-            raise("ERROR")
+            
+    else:
+        print("ERROR,please try again")
     
